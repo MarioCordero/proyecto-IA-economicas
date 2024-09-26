cf359a06cb4a0449356ecd450c25ea6a733f2e4c
spectprueba <143642679+MarioCordero@users.noreply.github.com>
2024-09-26 10:50:24 -0600

    Actualizando

--- src/fileManager.py
+++ src/fileManager.py
@@ -1,25 +1,12 @@
 import pandas as pd #Libreria para leer archivos xlsx
 
-def cargar_datos_xls(ruta_archivo):
-    df = pd.read_excel(ruta_archivo)
-    
-    actividades = []
-    
-    # Recorrer filas del archivo XLS y extraer columnas relevantes
-    for index, row in df.iterrows():
-        codigo_inscripcion = row['Código de inscripción']
-        nombre = row['Nombre']
-        fecha_inicio = row['Fecha de inicio']
-        fecha_fin = row['Fecha de fin']
-        antecedentes = row['Antecedentes']
-        
-        # Guardar cada actividad con sus antecedentes
-        actividades.append({
-            "codigo_inscripcion": codigo_inscripcion,
-            "nombre": nombre,
-            "fecha_inicio": fecha_inicio,
-            "fecha_fin": fecha_fin,
-            "antecedentes": antecedentes,
-        })
-    
-    return actividades
+def seleccionar_archivo(ventana, callback):
+    ruta_archivo = filedialog.askopenfilename(
+        title="Seleccionar archivo",
+        filetypes=(("Archivos Excel", "*.xlsx *.xls"), ("Todos los archivos", "*.*"))
+    )
+    if ruta_archivo:
+        label_ruta.config(text=f"Archivo seleccionado: {ruta_archivo}")
+        callback(ruta_archivo)  # Llama al callback con la ruta del archivo
+        ventana.destroy()  # Cierra la ventana
+    return ruta_archivo
--- src/main.py
+++ src/main.py
@@ -1,10 +1,31 @@
 import fileManager
 import indicadores
+import pandas as pd #Libreria para leer archivos xlsx
+import userInterface
+
 
 def main():
-    # Cargar los datos del archivo XLS
-    ruta_archivo = "../doc/Vigencias de proyecto.xlsx"  # Usa una raw string para evitar problemas con las barras invertidas
+    # Clase "controladora"
+
+    ruta_archivo = ""  # Usa una raw string para evitar problemas con las barras invertidas
+    ucr_bite = True
+
+    if(ucr_bite) :
+        userInterface.initializeUI(ruta_archivo)
+        df = pd.read_excel(ruta_archivo, skiprows=6)
+        # Iterar sobre las filas y columnas a partir de la fila 7
+        for index, row in df.iterrows():
+            codigo_inscripcion = row['Código de inscripción']
+            nombre = row['Nombre']
+            fecha_inicio = row['Fecha de inicio']
+            fecha_fin = row['Fecha de fin']
+            antecedentes = row['Antecedentes']
+
     # actividades = cargar_datos_xls(ruta_archivo)
+
+
+    # Imprimir los nombres de las columnas
+    print(df.columns)
 
     # Procesar las actividades
     # for actividad in actividades:
