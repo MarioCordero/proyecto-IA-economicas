950a545fd13eca58f72e3293c1a547c27d07e900
Mario Cordero <mariogabriel.cordero@ucr.ac.cr>
2024-10-27 21:03:37 -0600

    Actualizando modelo de UI

--- src/view/View.py
+++ src/view/View.py
@@ -1,21 +1,25 @@
-from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QMainWindow, QWidget, QSpacerItem, QSizePolicy, QStackedLayout, QApplication
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import QRect, Qt
-from PyQt5.uic import loadUi  # Importar loadUi desde PyQt5.uic
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.uic import loadUi
 
 class View(QMainWindow):
-
     def __init__(self, controller):
         super().__init__()
         self.controller = controller
-        self.initUI() # Initialize the User Interface
+        self.initUI()  # Initialize the User Interface
 
     def initUI(self):
+        loadUi( 'view/mainWindow.ui' , self )  # Load mainWindow.ui, designed in Qt Designer
         
-        loadUi('view/mainWindow.ui', self) # Load the main GUI, designed on QtDesigner
+        self.landingPage = QMainWindow() #Initiate the landingPage first, so the landingPage now gonna be a main window
+        loadUi( 'view/landingPage.ui' , self.landingPage ) # Load from the UI file
+        self.stackedWidget.addWidget(self.landingPage) # Add landingPage to the stackedWidget
+        self.stackedWidget.setCurrentWidget( self.landingPage ) # set the landingPage to the main window on the stackedWidget
 
-        # self.landingPage = QWidget # Create a QWidget to the landing page   
-        # loadUi('view/lanfingPage.ui',self.landingPage)        
+
+        # Load and add the databaseExplorer page
+        self.databaseExplorer = QMainWindow()  # Instantiate the databaseExplorer window
+        loadUi('view/databaseExplorer.ui', self.databaseExplorer)  # Load its UI layout
+        self.stackedWidget.addWidget(self.databaseExplorer)  # Add to the stacked widget
 
     def update_label(self, file_path):
         self.label_ruta.setText(f"Archivo seleccionado: {file_path}")
